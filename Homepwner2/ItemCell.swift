ccd7684505ddda2e3129173526f942ff6e72a617
Sarah Pell <spell1@uvm.edu>
2020-03-28 12:57:57 -0400

    more adds

---
+++ Homepwner2/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner2
+//
+//  Created by Sarah Pell on 3/28/20.
+//  Copyright © 2020 Sarah Pell. All rights reserved.
+//
+
+import UIKit
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
--- Homepwner2/ItemsViewController.swift
+++ Homepwner2/ItemsViewController.swift
@@ -52,8 +52,8 @@ class ItemsViewController: UITableViewController {
         // If the table view is asking to commit a delete command...
         if editingStyle == .delete {
             let item = itemStore.allItems[indexPath.row]
-            let title = "Delete \(item.title)?"
-            let message = "Are you sure you want to delete this item?"
+            let title = "Remove \(item.title)?"
+            let message = "Are you sure you want to delete this adoption listing?"
             let ac = UIAlertController(title: title,
                                        message: message,
                                        preferredStyle: .actionSheet)
@@ -73,38 +73,7 @@ class ItemsViewController: UITableViewController {
                 
         }
     }
-//    override func tableView(_ tableView: UITableView,
-//                            commit editingStyle: UITableViewCell.EditingStyle,
-//                            forRowAt indexPath: IndexPath) {
-//         // If the table view is asking to commit a delete command...
-//        if editingStyle == .delete {
-//            let item = itemStore.allItems[indexPath.row]
-//            let title = "Delete \(item.title)?"
-//            let message = "Are you sure you want to delete this item?"
-//            let ac = UIAlertController(title: title,
-//                                       message: message,
-//                                       preferredStyle: .actionSheet)
-//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//            ac.addAction(cancelAction)
-//            let deleteAction = UIAlertAction(title: "Delete",
-//                                             style: .destructive,handler: { (action) -> Void in
-//                                                // Remove the item from the store
-//                                                self.itemStore.removeItem(item)
-//                                                // Also remove that row from the table view with an animation
-//                                                self.tableView.deleteRows(at: [indexPath], with: .automatic)
-//
-//            })
-//            ac.addAction(deleteAction)
-//            // Present the alert controller
-//            present(ac, animated: true, completion: nil)
-//            // Remove the item from the store
-//            itemStore.removeItem(item)
-//            // Also remove that row from the table view with an animation
-//            tableView.deleteRows(at: [indexPath], with: .automatic)
-//        }
-//    }
 
-    
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -113,15 +82,16 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
-                                                 for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",for: indexPath) as! ItemCell
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableview
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.title
-        cell.detailTextLabel?.text = "$\(item.adoptionFee)"
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.title
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.adoptionFee)"
+        
         return cell
     }
     
@@ -132,5 +102,6 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        tableView.rowHeight = 65
     }
 }
