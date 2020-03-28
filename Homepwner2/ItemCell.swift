910a5ba6af4d841b92f99d2dcd89d8982bb640e6
Sarah Pell <spell1@uvm.edu>
2020-03-28 15:54:17 -0400

    segue added for detail

--- Homepwner2/ItemCell.swift
+++ Homepwner2/ItemCell.swift
@@ -11,4 +11,30 @@ class ItemCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
+    
+    func updateValueLabelTextColor() {
+        if let text = valueLabel.text {
+            let startIndex = text.index(text.startIndex, offsetBy: 1)
+            if let value = Double(text.substring(from: startIndex)) {
+
+                if value < 50 {
+                    valueLabel.textColor = UIColor.green
+                } else {
+                    valueLabel.textColor = UIColor.red
+                }
+            }
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateValueLabelTextColor()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
 }
