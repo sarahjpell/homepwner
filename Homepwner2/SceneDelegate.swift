e984a0de2802cbba84c9edd53538f6aec5e92e32
Sarah Pell <spell1@uvm.edu>
2020-03-28 17:48:21 -0400

    title for app //navigation

--- Homepwner2/SceneDelegate.swift
+++ Homepwner2/SceneDelegate.swift
@@ -20,9 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //         Create an ItemStore
         let itemStore = ItemStore()
         // Access the ItemsViewController and set its item store
-        let itemsController = window!.rootViewController as! ItemsViewController
+        let navController = window!.rootViewController as! UINavigationController
+        let itemsController = navController.topViewController as! ItemsViewController
         itemsController.itemStore = itemStore
-        
         guard let _ = (scene as? UIWindowScene) else { return }
         
     }
