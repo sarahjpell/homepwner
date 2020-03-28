2b5be3219dae13083a8773ab33bd133e6b993cd7
Sarah Pell <spell1@uvm.edu>
2020-03-28 17:39:23 -0400

    more stuff

---
+++ Homepwner2/DetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  DetailViewController.swift
+//  Homepwner2
+//
+//  Created by Sarah Pell on 3/28/20.
+//  Copyright © 2020 Sarah Pell. All rights reserved.
+//
+
+import UIKit
+class DetailViewController: UIViewController, UITextFieldDelegate {
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameField.text = item.title
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.adoptionFee))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Clear first responder
+        view.endEditing(true)
+        // "Save" changes to item
+        item.title = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text
+        if let valueText = valueField.text, let value = numberFormatter.number(from: valueText) {
+            item.adoptionFee = value.intValue
+        } else {
+            item.adoptionFee = 0
+        }
+    }
+}
