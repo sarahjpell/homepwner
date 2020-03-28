68330337ecf432eb0a462b37985d742a228769db
Sarah Pell <spell1@uvm.edu>
2020-03-28 17:57:32 -0400

    got rid of buttons, all in navigation bar now

--- Homepwner2/DetailViewController.swift
+++ Homepwner2/DetailViewController.swift
@@ -16,7 +16,11 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
     
-    var item: Item!
+    var item: Item!{
+        didSet {
+            navigationItem.title = item.title
+        }
+    }
 
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
